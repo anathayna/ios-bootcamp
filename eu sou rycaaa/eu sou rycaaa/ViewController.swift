e846c5234f2aea033bb9f7b3485d386d14a07653
anathayna <anathaynafranca@gmail.com>
2020-03-19 22:20:53 -0300

    adding xcode project

---
+++ eu sou rycaaa/eu sou rycaaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eu sou rycaaa
+//
+//  Created by Ana Thayna Franca on 19/03/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
