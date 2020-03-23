8fadf7d5f7865ba1baa9adc83f6754cdf920d7bc
anathayna <anathaynafranca@gmail.com>
2020-03-23 17:02:07 -0300

    adding i am poor project

---
+++ i am poor/i am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Ana Thayna Franca on 23/03/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
