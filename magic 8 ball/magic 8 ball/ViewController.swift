384f0fc47cab1e299e601da5e942152531adfb7a
anathayna <anathaynafranca@gmail.com>
2020-03-31 19:24:26 -0300

    adding magic 8 ball project

---
+++ magic 8 ball/magic 8 ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic 8 ball
+//
+//  Created by Ana Thayna Franca on 31/03/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
