2bae894a694224ae97c6f1f58a66311a792f80b0
anathayna <anathaynafranca@gmail.com>
2020-04-13 13:22:25 -0300

    adding different sounds

--- xylophone/xylophone/ViewController.swift
+++ xylophone/xylophone/ViewController.swift
@@ -18,12 +18,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        //print(sender.currentTitle)
-        playSound()
+        playSound(soundName: sender.currentTitle!)
+        }
     }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player!.play()
                 
