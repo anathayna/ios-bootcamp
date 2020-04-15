e0627c1d564a572f1a30a7d2d6e3cdd5c866e454
anathayna <anathaynafranca@gmail.com>
2020-04-15 12:26:14 -0300

    adding egg timer project

---
+++ egg timer/egg timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  egg timer
+//
+//  Created by Ana Thayna Franca on 15/04/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
