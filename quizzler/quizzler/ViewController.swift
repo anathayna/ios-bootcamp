fedfa5e70359f631b1b227b397fddfd439934dce
anathayna <anathaynafranca@gmail.com>
2020-04-25 14:28:07 -0300

    adding new questions

--- quizzler/quizzler/ViewController.swift
+++ quizzler/quizzler/ViewController.swift
@@ -18,9 +18,18 @@ class ViewController: UIViewController {
     var questionNumber = 0
     
     let quiz = [
-        Question(q: "Four + Two is equal to Six", a: "True"),
-        Question(q: "Five - Three is greater than One", a: "True"),
-        Question(q: "Three - Eight is less than Ten", a: "False")
+        Question(q: "O sangue da lesma é verde", a: "True"),
+        Question(q: "Aproximadamente um quarto dos ossos humanos estão nos pés", a: "True"),
+        Question(q: "A área total da superfície de dois pulmões humanos é de aproximadamente 70 metros quadrados", a: "True"),
+        Question(q: "Na Virgínia, Estados Unidos, se você acidentalmente bater em um animal com seu carro, poderá levá-lo para casa para comer", a: "True"),
+        Question(q: "Thomas Edison foi o primeiro inventor da lâmpada incandescente", a: "False"),
+        Question(q: "É ilegal fazer xixi no oceano em Portugal", a: "True"),
+        Question(q: "Você pode levar uma vaca escada abaixo, mas não escada acima", a: "False"),
+        Question(q: "O Google era originalmente chamado de 'Backrub'", a: "True"),
+        Question(q: "Saara é o maior deserto do mundo", a: "False"),
+        Question(q: "O som mais alto produzido por qualquer animal é de 188 decibels. Esse animal é o elefante africano", a: "False"),
+        Question(q: "Nenhum pedaço de papel quadrado pode ser dobrado ao meio mais de 7 vezes", a: "False"),
+        Question(q: "Daniel Rutherford descobriu um elemento que ficou conhecido como “ar venenoso”. Esse elemento é o Nitrogênio", a: "True")
     ]
     
     override func viewDidLoad() {
@@ -37,9 +46,9 @@ class ViewController: UIViewController {
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
-            print("Right!!")
+            sender.backgroundColor = UIColor.green
         } else {
-            print("Wrong!!")
+            sender.backgroundColor = UIColor.red
         }
         
         if questionNumber + 1 < quiz.count {
@@ -48,11 +57,13 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
 
 }
