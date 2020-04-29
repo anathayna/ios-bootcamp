7655600ff1655de2acc8e90bffdbd6d42b7b0d31
anathayna <anathaynafranca@gmail.com>
2020-04-28 21:13:57 -0300

    adding UISlider

--- imc/imc/Controller/ViewController.swift
+++ imc/imc/Controller/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var weightLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        //print(String(format: "%.2fm", sender.value))
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSlider(_ sender: UISlider) {
+        //print(Int(sender.value))
+        //print(String(format: "%.0fkg", sender.value))
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+    }
 }
 
