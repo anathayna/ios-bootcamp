8adffbf04bb28b1eec04350c33eb853cfec59833
anathayna <anathaynafranca@gmail.com>
2020-05-01 15:40:08 -0300

    adding second vc

---
+++ imc/imc/Controller/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  imc
+//
+//  Created by Ana Thayna Franca on 01/05/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit //Framework
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
