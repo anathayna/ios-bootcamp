d8972d6ee6cc6fe3a5d7b88a55808110cf03ea90
anathayna <anathaynafranca@gmail.com>
2020-05-05 12:13:51 -0300

    adding tipsy project

---
+++ tipsy/tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Ana Thayna Franca on 05/05/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
