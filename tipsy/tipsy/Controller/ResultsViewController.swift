f9917a71b1178eab4776d4273a36a240e7ad5445
anathayna <anathaynafranca@gmail.com>
2020-05-05 12:51:15 -0300

    finish tipsy project

---
+++ tipsy/tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultsViewController.swift
+//  tipsy
+//
+//  Created by Ana Thayna Franca on 05/05/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    var result = "0.0"
+    var tip = 10
+    var split = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        totalLabel.text = result
+        settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+
+}
