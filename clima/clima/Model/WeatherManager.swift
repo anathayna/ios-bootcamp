c56a0a377d8afefc34e7ae608935af17b31fad35
anathayna <anathaynafranca@gmail.com>
2020-05-12 12:54:57 -0300

    adding url session

--- clima/clima/Model/WeatherManager.swift
+++ clima/clima/Model/WeatherManager.swift
@@ -13,10 +13,41 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=d712dea299c988a43cbe05fdc55ff6ce&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=d712dea299c988a43cbe05fdc55ff6ce&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create URL
+        
+        if let url = URL(string: urlString) {
+            // 2. Create a URLSession
+            
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give session task
+            
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            // 4. Start task
+            
+            task.resume()
+            
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
