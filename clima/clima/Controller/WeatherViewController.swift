0de224f01f10fbdfbdaaca4ee55f62fde677cc3e
anathayna <anathaynafranca@gmail.com>
2020-05-14 12:50:33 -0300

    adding dispatch queue

--- clima/clima/Controller/WeatherViewController.swift
+++ clima/clima/Controller/WeatherViewController.swift
@@ -52,7 +52,13 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     }
     
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-        print(weather.temperature)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionalName)
+        }
+        
+        //weather
+        //print(weather.temperature)
     }
     
     func didFailWithError(error: Error) {
