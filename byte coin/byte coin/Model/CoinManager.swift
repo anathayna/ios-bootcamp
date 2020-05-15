c6eb9480e52f5b5b621685bfde339ad7530586cd
anathayna <anathaynafranca@gmail.com>
2020-05-15 19:48:31 -0300

    adding layout

---
+++ byte coin/byte coin/Model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  byte coin
+//
+//  Created by Ana Thayna Franca on 15/05/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+}
