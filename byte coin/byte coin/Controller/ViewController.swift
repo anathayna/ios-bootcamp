5e4b5acf7b5846eb7722df703c3e05de8cd8d474
anathayna <anathaynafranca@gmail.com>
2020-05-18 12:57:35 -0300

    adding picker view

--- byte coin/byte coin/Controller/ViewController.swift
+++ byte coin/byte coin/Controller/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet var bitcoinLabel: UILabel!
+    @IBOutlet var currencyLabel: UILabel!
+    @IBOutlet var currencyPicker: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+        print(coinManager.currencyArray[row])
+        print(row)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
     }
 
 }
-
