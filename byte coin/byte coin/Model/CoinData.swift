c6ff066ea79e4ee74923b7f3c2ac7cc0e37b53cd
anathayna <anathaynafranca@gmail.com>
2020-05-19 12:36:00 -0300

    parsing JSON data

---
+++ byte coin/byte coin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  byte coin
+//
+//  Created by Ana Thayna Franca on 19/05/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
