9f64245f2b2c9a726d178de47cfb8d26842de64c
anathayna <anathaynafranca@gmail.com>
2020-06-02 13:21:52 -0300

    adding bussiness card project

---
+++ ana thayna/ana thayna/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  ana thayna
+//
+//  Created by Ana Thayna Franca on 02/06/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50.0)
+            .foregroundColor(.white)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.black)
+                Text(text)
+            })
+            .padding()
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hellou?", imageName: "phone.circle")
+            .previewLayout(.sizeThatFits)
+    }
+}
