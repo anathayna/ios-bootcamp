e5defcfb8000f95eea5ef7e8ef97b6bab4c6daa0
anathayna <anathaynafranca@gmail.com>
2020-06-08 13:05:34 -0300

    adding table view controller

---
+++ todoey/todoey/TodoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  todoey
+//
+//  Created by Ana Thayna Franca on 06/06/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+}
