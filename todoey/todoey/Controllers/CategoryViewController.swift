fda34a30bb35e59f32a2a48f74eebf1dda0dc565
anathayna <anathaynafranca@gmail.com>
2020-06-16 12:42:11 -0300

    adding category table view

---
+++ todoey/todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryViewController.swift
+//  todoey
+//
+//  Created by Ana Thayna Franca on 16/06/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - TableView Datasource Methods
+    
+    
+    
+    // MARK: - Data Manipulation Methods
+
+    @IBAction func addButtonPress(_ sender: UIBarButtonItem) {
+    }
+    
+
+    
+    // MARK: - TableView Delegate Methods
+}
