d246b50167d1424b06425acc87604fc877b3063f
anathayna <anathaynafranca@gmail.com>
2020-07-14 12:32:13 -0300

    restarting project

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
     }
 
 }
+
