4677cd5851aa18dd481b026b056d762cb1ceedad
anathayna <anathaynafranca@gmail.com>
2020-07-17 11:57:08 -0300

    adding leilao project

---
+++ leilao/leilao/Models/Avaliador.swift
@@ -0,0 +1,37 @@
+//
+//  Avaliador.swift
+//  leilao
+//
+//  Created by Ana Thayna Franca on 17/07/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import Foundation
+
+class Avaliador {
+    
+    private var maiorDeTodos = Double.leastNonzeroMagnitude
+    private var menorDeTodos = Double.greatestFiniteMagnitude
+    
+    func avalia(leilao:Leilao) {
+        guard let lances = leilao.lances else { return }
+        
+        for lance in lances {
+            if lance.valor > maiorDeTodos {
+                maiorDeTodos = lance.valor
+            }
+            else if lance.valor < menorDeTodos {
+                menorDeTodos = lance.valor
+            }
+        }
+    }
+    
+    func maiorLance() -> Double {
+        return maiorDeTodos
+    }
+    
+    func menorLance() -> Double {
+        return menorDeTodos
+    }
+    
+}
---
+++ leilao/leilaoTests/leilaoTests.swift
@@ -0,0 +1,34 @@
+//
+//  leilaoTests.swift
+//  leilaoTests
+//
+//  Created by Ana Thayna Franca on 17/07/20.
+//  Copyright © 2020 Ana Thayna Franca. All rights reserved.
+//
+
+import XCTest
+@testable import leilao
+
+class leilaoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
