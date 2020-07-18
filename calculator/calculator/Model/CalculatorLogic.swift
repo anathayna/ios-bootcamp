ca4d77e20f6e28ce7c87d53e01b309cd684ded5f
anathayna <anathaynafranca@gmail.com>
2020-07-17 23:52:49 -0300

    adding operators

--- calculator/calculator/Model/CalculatorLogic.swift
+++ calculator/calculator/Model/CalculatorLogic.swift
@@ -11,12 +11,13 @@ import Foundation
 struct CalculatorLogic {
     
     private var number: Double?
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
     
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         if let n = number {
             if symbol == "+/-" {
                 return n * -1
@@ -24,8 +25,23 @@ struct CalculatorLogic {
                 return 0
             } else if symbol == "%" {
                 return n * 0.01
+            } else if symbol == "=" {
+                return performTwoNumCalculation(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
             }
         }
         return nil
     }
+    
+    private func performTwoNumCalculation(n2: Double) -> Double {
+        if let n1 = intermediateCalculation?.n1,
+            let operation = intermediateCalculation?.calcMethod {
+            
+            if operation == "+" {
+                return n1 + n2
+            }
+        }
+    }
+    
 }
