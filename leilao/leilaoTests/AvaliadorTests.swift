ac6c492a80fb8620861b628751185ca7b9684f23
anathayna <anathaynafranca@gmail.com>
2020-08-01 19:50:58 -0300

    adding new tests

--- leilao/leilaoTests/AvaliadorTests.swift
+++ leilao/leilaoTests/AvaliadorTests.swift
@@ -36,4 +36,16 @@ class AvaliadorTests: XCTestCase {
         XCTAssertEqual(400.0, leiloeiro.maiorLance())
     }
 
+    func testDeveEntenderLeilaoApenasUmLance() {
+        let joao = Usuario(nome: "Joao")
+        let leilao = Leilao(descricao: "Playstation 4")
+        leilao.propoe(lance: Lance(joao, 1000.0))
+        
+        let leiloeiro = Avaliador()
+        leiloeiro.avalia(leilao: leilao)
+        
+        XCTAssertEqual(1000.0, leiloeiro.menorLance())
+        XCTAssertEqual(1000.0, leiloeiro.maiorLance())
+    }
+
 }
