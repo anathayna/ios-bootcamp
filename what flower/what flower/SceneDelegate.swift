f7c4b2187774efd8a33b68c3d3e5fc409748ad20
ana thayna <anathaynafranca@gmail.com>
2021-07-14 22:31:58 -0300

    adding what flower project

---
+++ what flower/what flower/SceneDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  SceneDelegate.swift
+//  what flower
+//
+//  Created by ana thayna on 14/07/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+
+}
+
---
+++ what flower/what flower/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  what flower
+//
+//  Created by ana thayna on 14/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
