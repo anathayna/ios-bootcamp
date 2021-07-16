ddeb314c77b5e77df43ad9306ac2b477f048e912
ana thayna <anathaynafranca@gmail.com>
2021-07-15 23:43:48 -0300

    adding image picker

--- what flower/what flower/ViewController.swift
+++ what flower/what flower/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet private weak var imageView: UIImageView?
+    
+    private let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let userPickerImage = info[UIImagePickerController.InfoKey.editedImage]
+        imageView?.image = userPickerImage as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction private func onTapCamera(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
     }
 
 }
-
